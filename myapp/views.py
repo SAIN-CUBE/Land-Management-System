87989a1d0100f91b4cc62898f5d0a2fdcb28901c
Abdul Hannan <121096664+techcoder01@users.noreply.github.com>
2024-02-16 09:57:14 +0500

    Updated Now

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,3 @@
-# myapp/views.py
-
 from django.shortcuts import render, redirect
 from .forms import LandOwnershipDataForm, OwnerForm, LandForm, TransactionForm
 from folium import Map, Marker, FeatureGroup, LayerControl
@@ -9,8 +7,8 @@ def home(request):
     if request.method == 'POST':
         # Assuming you have a button in the home page to initiate the redirection
         return redirect('land_management_data')
-
-    return render(request, 'home.html', {'land_management_data_url': 'land_management_data'})
+    else:
+        return render(request, 'home.html', {'land_management_data_url': 'land_management_data'})
 
 def land_management_data(request):
     m = Map(location=[40.7128, -74.0060], zoom_start=12)  # Initialize map outside the if-else block
@@ -35,30 +33,8 @@ def land_management_data(request):
 
             land_ownership_data.save()
 
-            # Render the map in the same template along with the form
-            streetlights = MyStreetlightModel.objects.all()
-            land_locations = MyLandLocationModel.objects.all()
-
-            streetlight_group = FeatureGroup(name="Streetlights")
-            land_location_group = FeatureGroup(name="Land Locations")
-
-            for streetlight in streetlights:
-                streetlight_group.add_child(Marker(location=[streetlight.latitude, streetlight.longitude], popup=streetlight.details))
-
-            for land_location in land_locations:
-                land_location_group.add_child(Marker(location=[land_location.latitude, land_location.longitude], popup=land_location.info))
-
-            m.add_child(streetlight_group)
-            m.add_child(land_location_group)
-            m.add_child(LayerControl())
-
-            return render(request, 'land_management_data.html', {
-                'land_ownership_form': land_ownership_form,
-                'owner_form': owner_form,
-                'land_form': land_form,
-                'transaction_form': transaction_form,
-                'map': m._repr_html_(),  # Pass the map content to the template
-            })
+            # Redirect to the success page upon successful form submission
+            return redirect('success')
 
     else:
         land_ownership_form = LandOwnershipDataForm()
@@ -75,5 +51,5 @@ def land_management_data(request):
     })
 
 def success(request):
-    # Your view logic goes here
+    # Render the success page
     return render(request, 'success.html')
