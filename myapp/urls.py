f9198d5612d664adc917a36fb4485d9478306503
Adan Nazir <107835126+AdanNazir@users.noreply.github.com>
2024-03-05 11:00:33 +0500

    Revert "ui updated"
    This reverts commit c51b05e6693308c793107b4e0d3e3eed5a5e86ef.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,9 @@
 # myapp/urls.py
 from django.urls import path
 from .views import home, land_management_data, success
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),  # Set the default home page
     path('land_management_data/', land_management_data, name='land_management_data'),
     path('success/', success, name='success'),
-
 ]
